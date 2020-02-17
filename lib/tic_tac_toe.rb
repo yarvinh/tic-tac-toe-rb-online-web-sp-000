70f70ae7abcc3104b9576d864e8d4731ada06755
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-17 04:09:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -151,6 +151,8 @@ def play(board)
         counter += 1
       if over?() == false
           turn(board)
+          over?(board)
+          winner(board)
       elsif over?(board)
           if winner(board) == "O"
                puts "Congratulations O!"
